4552f1ede76fc32f75386d47dd6d0d863f938d1a
Вячеслав Вольхин <you@example.com>
2022-12-02 09:08:07 +0500

    add

--- js/scripts.js
+++ js/scripts.js
@@ -37,9 +37,21 @@ $(document).ready(function(){
             nextArrow: '<span class="btn-action-ico ico-arrow ico-arrow-main ico-arrow-next"></span>',
             responsive: [
                 {
-                    breakpoint: 1200,
+                    breakpoint: 1024,
                     settings: {
                         slidesToShow: 3,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 640,
+                    settings: {
+                        slidesToShow: 1,
                     }
                 },
             ]
